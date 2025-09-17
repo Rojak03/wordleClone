ef5c69b0e85f0b7abf3dab185557b8f786b0be20
Rojak03 <chrisnj.ftw@gmail.com>
2025-09-17 09:20:33 +0200

    updated app.js
    added letters to boxes,
    added function to removed current letter when backspace is pressed

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-document.addEventListener("keyup", (event) => { 
-    console.log(event);
-    if (isLetter(event.key)){
-      handleInput(event.key);
-    }
-    else if (event.key === "Backspace"){
-    }
-    else if (event.key === "Enter"){
-    }
+const ANSWER_LENGTH = 5;
+const ROUNDS = 6;
+const letters = document.querySelectorAll(".letter-box");
+const loadingDiv = document.querySelector(".info-bar");
+let currentGuess = "";
+let currentRound = 0;
+let done = false;
+function init() {
+
+  getWordOfTheDay();
+}
+document.addEventListener("keyup", (event) => {
+  console.log(event);
+  if (isLetter(event.key)) {
+    handleInput(event.key);
+  }
+  else if (event.key === "Backspace") {
+    backspace();
+  }
+  else if (event.key === "Enter") {
+    // submit word
+
+  }
 
 
 })
+
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
-function handleInput(key){
+function handleInput(letter) {
+  if (currentGuess.length < ANSWER_LENGTH) {
+    currentGuess += letter;
+  } else {
+    current = currentGuess.substring(0, currentGuess.length - 1) + letter;
+  }
 
-
+  letters[currentRound * ANSWER_LENGTH + currentGuess.length - 1].innerText =
+    letter;
 }
+function backspace() {
+  currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+  letters[currentRound * ANSWER_LENGTH + currentGuess.length].innerText = "";
+}
+function submitGuess() {
+  if (currentGuess.length !== ANSWER_LENGTH) {
+    return;
+  }
+  //check if word is real
+  let wordOfTheDay;
+  async function getWordOfTheDay() {
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const data = await res.json();
+    wordOfTheDay = data.word.toUpperCase();
+    console.log(wordOfTheDay);
+  }
+
+  init();
